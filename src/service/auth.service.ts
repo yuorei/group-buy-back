b8ad1e059b57500134340a5b0143afec79970188
yuorei <yuorei71@gmail.com>
2023-08-10 00:43:53 +0900

    サービスの移行

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { UsersService } from '../users/users.service';
+import { UsersService } from './users.service';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
@@ -7,7 +7,7 @@ export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService
-  ) {}
+  ) { }
 
   async signIn(username, pass) {
     const user = await this.usersService.findOne(username);
