783b7b602ff17e34e699cc75152b268dc3d083ff
tsoushi <79128170+tsoushi@users.noreply.github.com>
2023-08-11 13:46:33 +0900

    createRecruitmentを追加

--- src/db/recruitment.ts
+++ src/db/recruitment.ts
@@ -2,6 +2,40 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+interface CreateRecruitmentInput {
+  userId: number;
+  groupId?: number;
+  name: string;
+  description: string;
+  maxMember?: number;
+  price: number;
+  deadline?: Date;
+}
+
+export const createRecruitment = async ({
+  userId,
+  groupId,
+  name,
+  description,
+  maxMember,
+  price,
+  deadline,
+}: CreateRecruitmentInput) => {
+  const recruitment = prisma.recruitment.create({
+    data: {
+      userId,
+      groupId,
+      name,
+      description,
+      maxMember,
+      price,
+      deadline,
+    },
+  });
+
+  return recruitment;
+};
+
 interface FindRecruitmentsInput {
   name: string;
   groupId: number;
