56fd92e1b1cdeec8f44fa9f647453f427aaec436
yuorei <yuorei71@gmail.com>
2023-08-11 13:48:13 +0900

    interfaceで定義

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,8 +1,8 @@
-export type UserResult = {
+export interface UserResult {
     id: number;
     name: string;
     display_name: string;
     password: string;
     createdAt: Date;
     updatedAt: Date;
-} | undefined;
+}
