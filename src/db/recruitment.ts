3724412d099f491163b63c81cf6ceb6a61576a71
yuorei <yuorei71@gmail.com>
2023-08-12 02:28:17 +0900

    exportする

--- src/db/recruitment.ts
+++ src/db/recruitment.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-interface CreateRecruitmentInput {
+export interface CreateRecruitmentInput {
   userId: number;
   groupId?: number;
   name: string;
